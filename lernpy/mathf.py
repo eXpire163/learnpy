6fdd323353305e7cd55bb30f77f9c272bb1c9dc0
CWIEDEMA <christian.wiedemann@capgemini.com>
2017-05-24 11:59:09 +0200

    base app

---
+++ lernpy/mathf.py
@@ -0,0 +1,9 @@
+
+
+class Mathf(object):
+    '''Mathf from unity clone'''
+    @staticmethod
+    def add(para_a, para_b):
+        '''adds 2 numbers'''
+        return para_a + para_b
+
---
+++ tests/test_mathf.py
@@ -0,0 +1,5 @@
+
+from .context import lernpy
+
+def add_test():
+    assert Mathf.add(1, 1) == 2
